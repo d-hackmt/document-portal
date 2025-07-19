3132416a2db6160855250511f994da2149b3c86e
Divesh <113240252+d-hackmt@users.noreply.github.com>
2025-07-19 16:33:02 +0530

    structure_updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setting llm and embedding models
+
+from setuptools import setup , find_packages
+
+setup(
+    name = "document_portal",
+    author = "dmt xpress",
+    maintainer_email="djadhwani1025@gmail.com",
+    version = "0.1",
+    packages=find_packages(),
+)
