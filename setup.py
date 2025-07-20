8bef678ee4ec7d1891a6368d82740ad17a31ece8
Divesh <113240252+d-hackmt@users.noreply.github.com>
2025-07-20 15:03:46 +0530

    just changed autohor name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup , find_packages
 
 setup(
     name = "document_portal",
-    author = "dmt xpress",
+    author = "dmt_xpress",
     maintainer_email="djadhwani1025@gmail.com",
     version = "0.1",
     packages=find_packages(),
