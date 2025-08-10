99e6382d29ed7bd1c93661b4d59224c0b92c323d
Divesh <113240252+d-hackmt@users.noreply.github.com>
2025-08-10 15:49:17 +0530

    added api app

--- api/main.py
+++ api/main.py
@@ -1,4 +1,75 @@
-## for all end points
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException, Request
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+from typing import List, Optional, Any, Dict
 
 
 
+
+
+app = FastAPI(title="Documnet Portal API", version="0.1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+# serve static & templates
+app.mount("/static",  StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+
+
+@app.get("/",response_class=HTMLResponse)
+async def serve_ui(request: Request):
+    # templates/index.html ko render karega
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/health")
+def health() -> Dict[str, str]:
+    return {"status": "ok", "service": "document-portal"}
+
+@app.post("/analyze")
+async def analyze_document(file: UploadFile = File(...)) -> Any:
+    try:
+        pass
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Analysis failed: {e}")
+    
+@app.post("/compare")
+async def compare_documents(reference: UploadFile = File(...), actual: UploadFile = File(...)) -> Any:
+    try:
+        pass
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Comparison failed: {e}")
+
+@app.post("/chat/index")
+async def chat_build_index() -> Any:
+    try:
+        pass
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Indexing failed: {e}")
+    
+@app.post("/chat/query")
+async def chat_query():
+    try:
+        pass
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Query failed: {e}")
+
+# command for executing the fast api
+# uvicorn api.main:app --reload    
