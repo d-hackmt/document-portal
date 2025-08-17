320e3e1c266261828f3680d0f4eafe011bcb4e16
Divesh <113240252+d-hackmt@users.noreply.github.com>
2025-08-17 17:06:15 +0530

    day 10 deployment over

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
-# setting llm and embedding models
+from setuptools import setup, find_packages
+from pathlib import Path
 
-from setuptools import setup , find_packages
+def parse_requirements(filename):
+    with open(filename, encoding="utf-8") as f:
+        return [
+            line.strip()
+            for line in f
+            if line.strip() and not line.startswith("#") and not line.startswith("-e")
+        ]
 
 setup(
-    name = "document_portal",
-    author = "dmt_xpress",
-    maintainer_email="djadhwani1025@gmail.com",
-    version = "0.1",
-    packages=find_packages(),
+    name="document_portal",
+    author="Sunny Savita",
+    version="0.1",
+    description="An intelligent document analysis and comparison system powered by LLMs",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["tests*", "examples*"]),
+    include_package_data=True,
+    install_requires=parse_requirements("requirements.txt"),
+    extras_require={
+        "dev": ["pytest", "pylint", "ipykernel"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.10",
 )
